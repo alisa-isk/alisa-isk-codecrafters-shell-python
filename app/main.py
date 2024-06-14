fd6fb1a8732b5bb467589969ae4029af4c117b36
Alisa <157826128+alisa-isk@users.noreply.github.com>
2024-06-14 14:48:51 -0400

    try 2

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
-    command = input()
-    print(f"{command}: command not found\n")
+    user_input = input()
+    if user_input not in commands:
+        sys.stdout.write(f"{user_input}: command not found\n")
 
 
 if __name__ == "__main__":
