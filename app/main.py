b51f0289960376400959e5e57d8337b1c33f17ef
Alisa <157826128+alisa-isk@users.noreply.github.com>
2024-06-15 00:12:54 -0400

    echo 2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 import sys
 
+command_list = ["echo", "exit", "type"]
+
+
+def print_type(command):
+    command = command[len("type "):]
+    if command in command_list:
+        sys.stdout.write(f"{command} is a shell builtin\n")
+    else:
+        sys.stdout.write(f"{command}: command not found\n")
+
 
 def main():
     # Uncomment this block to pass the first stage
@@ -8,15 +18,17 @@ def main():
 
     # Wait for user input
     command = input()
+    valid_commands = {}
     if command == "exit 0":
         exit(0)
     elif command.startswith("echo "):
-        print(command[len("echo ") :])
+        print(command[len("echo "):])
+    elif command.startswith("type "):
+        print_type(command)
     else:
         sys.stdout.write(f"{command}: command not found\n")
 
     main()
-
 
 
 if __name__ == "__main__":
