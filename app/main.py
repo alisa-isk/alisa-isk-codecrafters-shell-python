d2e04284541c7891fe2e6f52daab7b3cfb64a571
Alisa <157826128+alisa-isk@users.noreply.github.com>
2024-06-23 16:20:10 -0400

    PATH try 1

--- app/main.py
+++ app/main.py
@@ -1,24 +1,40 @@
 import sys
+import os
 
 command_list = ["echo", "exit", "type"]
+PATH = os.environ.get("PATH")
+
+def get_file_path(PATH, file_name):
+    """helper function to get path of command"""
+    directories = PATH.split(":")
+    for directory in directories:
+        path = os.path.join(directory, file_name)
+        # Case when command is found
+        if os.path.isfile(path):
+            return path
+    return None
 
 
 def print_type(command):
+    """Prints the command type"""
     command = command[len("type "):]
     if command in command_list:
         sys.stdout.write(f"{command} is a shell builtin\n")
+    elif PATH:
+        file_name = get_file_path(PATH, command)
+        if file_name:
+            sys.stdout.write(f"{command} is {file_name}\n")
+        else:
+            sys.stdout.write(f"{command}: not found\n")
     else:
         sys.stdout.write(f"{command}: not found\n")
 
 
 def main():
-    # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
-    # Wait for user input
     command = input()
-    valid_commands = {}
     if command == "exit 0":
         exit(0)
     elif command.startswith("echo "):
